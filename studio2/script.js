43f6e2ab088bdcb9fdb10f074e415b283e6b9b72
jlgerhardt <jlgerhardt@ucdavis.edu>
2024-03-05 01:25:29 -0800

    studio2 tweaks

--- studio2/script.js
+++ studio2/script.js
@@ -127,11 +127,11 @@
     }
   });
 
-      // Event listener for the back button to navigate to the landing page
-    images.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelectorAll("main").className = "hidden";
-    });
+    //   // Event listener for the back button to navigate to the landing page
+    // images.addEventListener("click", function(e) {
+    //     e.preventDefault();
+    //     document.querySelectorAll("main").className = "hidden";
+    // });
 
     // // Event listener for clicking on the main story link to go to full page
     // const clickTwenty = document.querySelector("#twenty");
